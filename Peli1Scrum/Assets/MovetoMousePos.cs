c824d6b11ff904c01537a94fcf9770c8a8a751a3
Emma Vantaa <emma.vantaa@gmail.com>
2019-05-07 14:49:04 +0300

    3D malleja ja jonkunlainen ampuminen
    Paljon korjattavaa muta siinä on alku

---
+++ Peli1Scrum/Assets/MovetoMousePos.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovetoMousePos : MonoBehaviour
+{
+    public Camera cam;
+
+
+    Vector3 mPoint;
+    public GameObject Hand;
+    //public Rigidbody2D rb;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = Camera.main;
+
+        //rb.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+        Hand.transform.position = new Vector3(mPoint.x, mPoint.y, 0);
+        //EnableObjectCol();
+    }
+
+    //void EnableObjectCol()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        rb.isKinematic = false;
+    //    }
+
+    //}
+}
---
+++ Peli1Scrum/Assets/TestScripts/ShootingScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    public Rigidbody  bunnyPrefab; // Ammo
+    public Transform instantiateFrom; // Mistä se ammo tulee
+
+    public float fireRate = 0.5f;
+    private float nextFire = 0.0f;
+ 
+
+    public Camera Cam;
+
+   
+
+
+    private void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            if (Input.GetButtonDown("Fire1") && Time.time > nextFire) // Kun painat vas hiirinappia ("Fire1) On hiirien vasen nappi
+        {
+            nextFire = Time.time + fireRate;
+           
+                Shoot();
+     
+         }
+        
+
+
+
+
+        void Shoot()
+        {
+
+            Rigidbody instantiatebunPrefab;
+
+            instantiatebunPrefab = Instantiate(bunnyPrefab, instantiateFrom.position, instantiateFrom.rotation) as Rigidbody;
+           instantiatebunPrefab.AddForce(instantiateFrom.forward *- 2000);
+     
+
+          //  RaycastHit hit;
+          //if( Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit,range))
+          //  {
+
+          //  }
+        }
+          
+
+        }
+
+    }
+
