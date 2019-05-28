bf8d6ce4472b708aadde28cce2762317e1378c59
Emma Vantaa <emma.vantaa@gmail.com>
2019-05-28 14:11:06 +0300

    Ampumpinen
    Uusi yritys. :D

---
+++ Peli1Scrum/Assets/TestScripts/BunnyAmmo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyAmmo : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter(Collision col)
+    {
+        
+        if (col.gameObject.CompareTag("CarrotWall") || col.gameObject.CompareTag("Ground"))
+        {
+
+            Debug.Log("Hits Carrot wall");
+            Destroy(gameObject, 0.5f);
+        }
+    }
+
+}
+
