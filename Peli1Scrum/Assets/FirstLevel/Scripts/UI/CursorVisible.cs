7a4014be792e86e126f92c513d38a77ed40e28da
Joona <joona.janhunen@student.careeria.fi>
2019-06-12 16:49:31 +0300

    Nappeja voi painaa, kun pitää vasen control pohjassa

---
+++ Peli1Scrum/Assets/FirstLevel/Scripts/UI/CursorVisible.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Invector.CharacterController;
+
+public class CursorVisible : MonoBehaviour
+{
+    GameObject mc;
+    vThirdPersonCamera tp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mc = GameObject.Find("vThirdPersonController");
+        tp = mc.GetComponent<vThirdPersonCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            tp.lockCamera = true;
+            
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            tp.lockCamera = false;
+        }
+    }
+}
